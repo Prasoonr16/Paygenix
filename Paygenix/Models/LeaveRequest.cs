e1380d34aef6052caf7011acaa1eb86cf6b5a1b0
Abhishek Garg <83693299+Abhishek0712-ctrl@users.noreply.github.com>
2024-11-13 13:49:06 +0530

    Update LeaveRequest.cs

--- Paygenix/Models/LeaveRequest.cs
+++ Paygenix/Models/LeaveRequest.cs
@@ -23,6 +23,8 @@ namespace Paygenix.Models
 
             [Required]
             [MaxLength(50)]
+            [Required(ErrorMessage = "Status is required.")]
+            [RegularExpression(@"^(Approved|Not Approved)$", ErrorMessage = "Status must be either 'Approved' or 'Not Approved'.")]
             public string Status { get; set; }  
 
             public DateTime RequestDate { get; set; }  
